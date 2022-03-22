460e7ba47d59264d885ea5fad06dfcb0e5095f98
hakohumi <mihumihu32@gmail.com>
2022-03-22 23:20:45 +0900

    ポータブル用の実行ファイルを生成するように変更

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        win: {
+          target: [
+            {
+              target: 'portable', // 'zip', 'nsis', 'portable'
+              arch: ['x64'], // 'x64', 'ia32'
+            },
+          ],
+        },
+      },
+    },
+  },
 })
